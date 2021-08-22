85bc40ba9104d5ff8e17af1df6547f1be2e3ba3a
Rafael R Ramos R <rafael.ramosr@hotmail.com>
2021-08-22 11:20:24 -0500

    Función para peticiones GET

---
+++ src/js/services/http.js
@@ -0,0 +1,12 @@
+const API_URL = 'https://jsonplaceholder.typicode.com';
+
+export const getDataService = async (path) => {
+  try {
+    const response = await fetch(`${API_URL}${path}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    const message = `Ha ocurrido un error: ${error}`;
+    throw new Error(message);
+  }
+}
