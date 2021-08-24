281bb2dfa215ba5c93ffda698221af84020a0b7d
Rafael R Ramos R <rafael.ramosr@hotmail.com>
2021-08-23 23:31:32 -0500

    Archivo de configuracion con dos nuevas rutas

--- src/js/config/index.js
+++ src/js/config/index.js
@@ -4,10 +4,26 @@ export const globalConfig = {
       nombre: 'Nombre completo',
       id_municipio_residencia: 'Residencia',
       fecha_nacimiento: 'Fecha de nacimiento',
+    },
+    motive: {
+      motivo_desplazamiento: 'Motivo desplazamiento',
+    },
+    displaced: {
+      id_persona: 'Nombre completo',
+      id_municipio_desplazamiento: 'Lugar de desplazamiento',
+      fecha_desplazamiento: 'Fecha de desplazamiento',
     }
   },
   users: {
     getAllPath: 'get_all_personas',
     addPath: 'add_persona',
+  },
+  motive: {
+    getAllPath: 'get_all_motivo_desplazamiento',
+    addPath: 'add_motivo_desplazamiento',
+  },
+  displaced: {
+    getAllPath: 'get_all_desplazados',
+    addPath: 'add_desplazado',
   }
 }
