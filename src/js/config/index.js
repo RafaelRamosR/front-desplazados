b68355ea2ff96cd1828dd763288ade78b4b6fb44
Rafael R Ramos R <rafael.ramosr@hotmail.com>
2021-08-24 14:09:09 -0500

    Organizando variables globales

--- src/js/config/index.js
+++ src/js/config/index.js
@@ -1,38 +1,38 @@
 export const globalConfig = {
   cadrItemData: {
     users: {
-      id_municipio_residencia: 'Residencia',
-      fecha_nacimiento: 'Fecha de nacimiento',
-      nombre: 'Nombre completo',
+      id_municipio_residencia : 'Residencia',
+      fecha_nacimiento        : 'Fecha de nacimiento',
+      nombre                  : 'Nombre completo',
     },
     motive: {
-      motivo_desplazamiento: 'Motivo desplazamiento',
+      motivo_desplazamiento : 'Motivo desplazamiento',
     },
     displaced: {
-      id_persona: 'Nombre completo',
-      id_municipio_desplazamiento: 'Lugar de desplazamiento',
-      fecha_desplazamiento: 'Fecha de desplazamiento',
+      id_persona                  : 'Nombre completo',
+      id_municipio_desplazamiento : 'Lugar de desplazamiento',
+      fecha_desplazamiento        : 'Fecha de desplazamiento',
     }
   },
   users: {
-    createPath: 'add_persona',
-    readAllPath: 'get_all_personas',
-    readByIdPath: 'get_persona',
-    updatePath: '',
-    deletePath: 'delete_persona',
+    createPath   : 'add_persona',
+    readAllPath  : 'get_all_personas',
+    readByIdPath : 'get_persona',
+    updatePath   : 'update_persona',
+    deletePath   : 'delete_persona',
   },
   motive: {
-    createPath: 'add_motivo_desplazamiento',
-    readAllPath: 'get_all_motivo_desplazamiento',
-    readByIdPath: 'get_motivo_desplazamiento',
-    updatePath: '',
-    deletePath: 'delete_motivo_desplazamiento',
+    createPath   : 'add_motivo_desplazamiento',
+    readAllPath  : 'get_all_motivo_desplazamiento',
+    readByIdPath : 'get_motivo_desplazamiento',
+    updatePath   : 'update_persona',
+    deletePath   : 'delete_motivo_desplazamiento',
   },
   displaced: {
-    createPath: 'add_desplazado',
-    readAllPath: 'get_all_desplazados',
-    readByIdPath: 'get_desplazados',
-    updatePath: '',
-    deletePath: 'delete_desplazados',
+    createPath   : 'add_desplazado',
+    readAllPath  : 'get_all_desplazados',
+    readByIdPath : 'get_desplazados',
+    updatePath   : 'update_desplazados',
+    deletePath   : 'delete_desplazados',
   }
 }
